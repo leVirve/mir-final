6af6fc711ac8ebe4327d735843abb6ffa4d5ccc5
wayne391 <archer837@yahoo.com.tw>
2016-06-11 15:51:56 +0800

    Update segmentation_by_gt.m

--- segmentation_by_gt.m
+++ segmentation_by_gt.m
@@ -1,4 +1,4 @@
-function [songs] = segmentation_by_gt( listOfAnnotaions , listOfSongs)
+function [songs] = segmentation_by_gt(listOfAnnotaions)
 
 songs = cell(size(listOfAnnotaions));
 for i = 1:length(listOfAnnotaions)
@@ -10,29 +10,11 @@ for i = 1:length(listOfAnnotaions)
     for sgmt = 1:length(C{1})
         [tag, other] = parse_annotation_tag(C{3}{sgmt});
         s = struct(...
-            'audio', 0, ...
-            'fs',0,...
             'label', tag, ...
-            'start', C{1}(sgmt) / 100, ...
-            'end', C{2}(sgmt) / 100, ...
+            'range', [C{1}(sgmt) / 100, C{2}(sgmt) / 100], ...
             'other', other);
         ss{sgmt} = s;
     end
     songs{i} = ss;
-
 end
-
-for i = 1:length(listOfSongs)
-    [audio,fs] = audioread(listOfSongs{i});
-    info = songs{i};
-   
-    for sgmt = 1:length(info)
-        st = floor(info{sgmt}.start * fs);
-        ed = floor(info{sgmt}.end * fs);
-        if(st == 0) st = 1; end
-        info{sgmt}.audio = audio(st:ed);
-        info{sgmt}.fs = fs;
-    end
-    songs{i} = info;
-
 end
