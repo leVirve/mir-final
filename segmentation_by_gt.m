56e2d6a6137dfd672cb1589547d7ccd36fd8ad78
Salas <gae.m.project@gmail.com>
2016-06-11 15:55:21 +0800

    add a tab

--- segmentation_by_gt.m
+++ segmentation_by_gt.m
@@ -1,20 +1,21 @@
 function [songs] = segmentation_by_gt(listOfAnnotaions)
 
-songs = cell(size(listOfAnnotaions));
-for i = 1:length(listOfAnnotaions)
-    fh = fopen(listOfAnnotaions{i});
-    C = textscan(fh, '%f %f %[^\r\n]');
-    fclose(fh);
+    songs = cell(size(listOfAnnotaions));
+    for i = 1:length(listOfAnnotaions)
+        fh = fopen(listOfAnnotaions{i});
+        C = textscan(fh, '%f %f %[^\r\n]');
+        fclose(fh);
 
-    ss = cell(size(C{1}));
-    for sgmt = 1:length(C{1})
-        [tag, other] = parse_annotation_tag(C{3}{sgmt});
-        s = struct(...
-            'label', tag, ...
-            'range', [C{1}(sgmt) / 100, C{2}(sgmt) / 100], ...
-            'other', other);
-        ss{sgmt} = s;
+        ss = cell(size(C{1}));
+        for sgmt = 1:length(C{1})
+            [tag, other] = parse_annotation_tag(C{3}{sgmt});
+            s = struct(...
+                'label', tag, ...
+                'range', [C{1}(sgmt) / 100, C{2}(sgmt) / 100], ...
+                'other', other);
+            ss{sgmt} = s;
+        end
+        songs{i} = ss;
     end
-    songs{i} = ss;
-end
+
 end
