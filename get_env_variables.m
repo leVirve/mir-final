6d217035375ba2628aafd424d419f79212cf4153
HaleyWu <gogogo753951741963@gmail.com>
2016-06-13 13:17:57 +0800

    add libsvm path

--- get_env_variables.m
+++ get_env_variables.m
@@ -3,6 +3,7 @@ function [PATH_AUDITORY_TOOLBOX] = get_env_variables()
     PATH_AUDITORY_TOOLBOX = './toolbox/AuditoryToolbox';
     addpath('./toolbox/ChromaToolbox');
     addpath('./toolbox/MIRToolbox');
+    addpath('./toolbox/libsvm-3.21/matlab');
     % Must add AuditoryToolbox through VARIABLE, due to some hacky solution
     % for solving the overwriting buit-in function `spectrogram` by AuditoryToolbox
     addpath(PATH_AUDITORY_TOOLBOX);
