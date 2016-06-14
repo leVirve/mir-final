34d60c92f926370b5005e76403c2919c575cb1e2
Salas <gae.m.project@gmail.com>
2016-06-14 17:34:36 +0800

    [QAQ] again bug in listing files

--- listfile.m
+++ listfile.m
@@ -2,10 +2,13 @@ function [result] = listfile(path, rule, range)
 
     % if no specified rule, default: list all files under `path`
     if nargin == 1
-        rule = '/*.txt';
+        rule = '/*';
     end
     
     files = dir([path rule]);
+    if strcmp(files(1).name, '.') == 1
+        files(1:2) = [];
+    end
     result = fullfile(path, {files.name});
 
     % if specify sub-list range
